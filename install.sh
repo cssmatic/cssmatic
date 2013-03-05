4ee092ffcd1fb48b2d0331685915cad277772346
Joaquin Cuenca Abela <e98cuenc@gmail.com>
2013-03-05 20:40:27 +0100

    Make some fixes to the installation process

--- install.sh
+++ install.sh
@@ -4,30 +4,36 @@ set -e
 if [ $(whoami) != "cssmatic" ]; then
     echo "Run this script under the user cssmatic:"
     echo "$ useradd -d /home/cssmatic -m cssmatic"
+    echo "$ adduser cssmatic sudo"
+    echo "$ sudo -i -u cssmatic bash"
+    echo "$ mkdir /home/cssmatic/www && cd /home/cssmatic/www && git clone https://github.com/cssmatic/cssmatic.git"
+    exit 1
 fi
 
-apt-get install nginx
+sudo apt-get install nginx
 
 virtualenv --distribute $HOME/.virtualenvs/cssmatic
 source $HOME/.virtualenvs/cssmatic/bin/activate
 pip install -r requirements.txt
+pybabel compile -d translations
 
-sudo mkdir -p /opt/cache/cssmatic/proxy
-sudo ln -s `pwd`/etc/init/gunicorn.conf /etc/init/
+# Install gunicorn configuration
+sudo cp etc/init/cssmatic-gunicorn.conf /etc/init/
 sudo initctl reload-configuration
 if ! curl --silent --head http://localhost:5000/ | egrep "^HTTP/1.0 200 OK"; then
     echo "Error installing cssmatic flask process"
-    exit 0
+    exit 1
 fi
 
-sudo ln -s `pwd`/etc/cssmatic-nginx.conf /etc/nginx/sites-enabled/
+# Install nginx configuration
+sudo mkdir -p /opt/cache/cssmatic/proxy
+sudo cp etc/nginx/sites-enabled/cssmatic-nginx.conf /etc/nginx/sites-enabled/
 if ! nginx -t; then
     echo "Error in nginx configuration"
-    exit 0
+    exit 1
 fi
-
 sudo nginx -s reload
 if ! curl --silent --head -H "Host: www.cssmatic.com" http://localhost/ | egrep "^HTTP/1.0 200 OK"; then
     echo "Error installing cssmatic nginx"
-    exit 0
+    exit 1
 fi
